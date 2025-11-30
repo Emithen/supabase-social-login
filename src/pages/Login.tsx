d7fbb5d0635db21052e1d9837088ed5ceecfd727
Emithen <86219540+Emithen@users.noreply.github.com>
2025-12-01 01:19:37 +0900

    feat: login 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,24 @@
+import supabase from "../supabase";
+
 const Login = () => {
+  const signInWithGithub = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: "http://localhost:5173/login",
+      },
+    });
+    console.log(data, error);
+  };
+
   return (
     <>
       <div className="text-blue-500 text-2xl">Login</div>
       <div className="text-green-500">Welcome to the Login Page!</div>
+
+      <button className="cursor-pointer bg-gray-800 text-white rounded-xl px-3 py-1 flex items-center justify-center" onClick={signInWithGithub}>Login With Github!</button>
     </>
-  )
-}
+  );
+};
 
 export default Login;
